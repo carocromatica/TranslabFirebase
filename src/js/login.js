fd64175b3de99ee7aeca1cd250e759fe6d46a86f
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-08-08 11:35:23 -0400

    se agregan validaciones de email a register con firebase

--- src/js/login.js
+++ src/js/login.js
@@ -3,7 +3,55 @@ function registerWithFirebase() {
   const mail = email.value;
   const pass = password.value;
 
-  firebase.auth().createUserWithEmailAndPassword(mail, pass)
+    if (mail.indexOf('@') < 0) {
+        alert("chan");
+        return false;
+    }
+
+    const tokens = mail.split('@');
+    if (tokens.length != 2) { //Verifica que el correo SÓLO tenga 2 partes
+        alert("chan");
+        return false; //Si es distinto de 2, será false
+    }
+
+    if (tokens[1].indexOf('.') < 0) {// si no tiene puntos
+        alert("chan");
+        return false;
+    }
+
+    if (tokens[0] == "") {// si no existe algo antes de la @
+        alert("chan");
+        return false
+    }
+
+    if (mail == "") {
+        alert("chan");
+        return false
+    }
+
+    const domTokens = tokens[1].split('.');
+    if (domTokens.length != 2) {
+        alert("chan");
+        return false;
+    }
+    if (domTokens[0].length < 1) {
+        alert("chan");
+        return false
+    }
+    if (domTokens[1].length < 1) {
+        alert("chan");
+        return false
+    }
+ // para detectar caracteres raros
+    const rejected = "!#$%^&*()+=-[]\\\';,/{}|\":<>?"
+    for (var i = 0; i < mail.length; i++) { // se parece al data dashboard :D
+        if (rejected.indexOf(mail.charAt(i)) != -1) {
+            alert("chan");
+            return false;
+        }
+    }
+ 
+     firebase.auth().createUserWithEmailAndPassword(mail, pass)
       .then((userData) => {
           firebase.database().ref(`usuarios/${userData.user.uid}`).set({
               username: name,
@@ -14,12 +62,14 @@ function registerWithFirebase() {
           window.location = "../src/home.html";
 
       })
+
+      
       .catch((error) => {
           console.log("Error de Firebase > Codigo > " + error.code);
           console.log("Error de Firebase > Mensaje > " + error.message);
 
       });
-}
+
 
 //Login
 function loginWithFirebase() {
@@ -43,5 +93,6 @@ function loginWithFirebase() {
           console.log("Error de firebase > Mensaje > " + error.message);
           alert("Revisa todos los datos ingresados. Correo y contraseña son obligatorios.");
       });
+}
 }
 
